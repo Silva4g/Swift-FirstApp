d497630d80c438d6c9c35a01674ed3770399e982
Kauan Silva <kauanssousa@gmail.com>
2024-06-29 21:43:54 -0300

    first commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Kauan sousa on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
